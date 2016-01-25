6e9e618282bdead13d4a91e9929938edc6eeb4d6
spache <v.mostepaniuk@drudesk.com>
2016-01-25 14:47:20 +0200

    Revert "Added untracked files"
    This reverts commit 9cfce158ff409e28c0ad01555d45b210d13366f5.

---
+++ sites/all/modules/custom/my_popup/my_popup_style.js
@@ -0,0 +1,41 @@
+<?php
+// Drupal.theme.prototype.my_custom_theme = function () {
+//   var html = '';
+//   html += '<div id="ctools-modal" class="popups-box my-first-popup">';
+//   html += ' <div class="ctools-modal-content my-popup ">';
+//   html += ' <span class="popups-close"><a class="close" href="#"></a></span>';
+//   html += ' <div class="modal-msg"></div>';
+//   html += ' <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+//   html += ' </div>';
+//   html += '</div>';
+//   return html;
+// }
+Drupal.theme.prototype.my_custom_theme = function () {
+  var html = '';
+  html += '<div id="ctools-modal" class="ctools-modal-custom">';
+  html += '  <div class="ctools-modal-content ctools-modal-custom-help-content">';
+  html += '    <div class="modal-header">';
+  html += '      <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
+  html += '      <div class="modal-title">Help</div>';
+  html += '    </div>';
+  html += '    <div class="modal-scroll"><div id="modal-content" class="modal-content modal-content-custom"></div></div>';
+  html += '  </div>';
+  html += '</div>';
+  return html;
+}
+/**
+* Provide the HTML to create the modal dialog.
+*/
+// (function ($) {
+// Drupal.theme.prototype.my_custom_theme = function () {
+//   var html = '';
+//   html += '<div id="ctools-modal" class="popups-box">';
+//   html += '  <div class="ctools-modal-content my-popup">';
+//   html += '    <span class="popups-close"><a class="close" href="#">' + Drupal.CTools.Modal.currentSettings.closeImage + '</a></span>';
+//   html += '    <div class="modal-scroll"><div id="modal-content" class="modal-content popups-body"></div></div>';
+//   html += '  </div>';
+//   html += '</div>';
+//   return html;
+// }
+// })(jQuery);
+// ?>
